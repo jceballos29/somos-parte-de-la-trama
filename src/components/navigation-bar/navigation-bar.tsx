7fcb788d858597a188464694f67cd591fb1993aa
Juan Ceballos <jacu29@gmail.com>
2023-11-19 20:31:28 -0500

    [FIX] - try a new navbar

--- src/components/navigation-bar/navigation-bar.tsx
+++ src/components/navigation-bar/navigation-bar.tsx
@@ -8,35 +8,37 @@ const NavigationBar: React.FC<NavigationBarProps> = () => {
 	const location = useLocation();
 
 	return (
-		<nav className='fixed z-10 bottom-2 left-1/2 -translate-x-1/2 rounded-full px-2 py-2 bg-white/40 backdrop-blur'>
-			<ul className='flex items-center justify-center space-x-4 duration-100 transition-all '>
-				{pages.map((page) => (
-					<Link
-						key={page.name}
-						to={page.url}
-						className={`flex items-center space-x-1 p-2 rounded-full overflow-hidden ${
-							location.pathname === page.url
-								? 'text-slate-600 bg-[#dbb198] w-24 justify-center'
-								: 'text-slate-950 w-9'
-						} duration-100 transition-all`}
-					>
-						{location.pathname === page.url ? (
-							<page.icons.solid className='h-5 w-5 duration-100 transition-all' />
-						) : (
-							<page.icons.outline className='h-5 w-5 duration-100 transition-all' />
-						)}
-						<span
-							className={`text-xs capitalize ${
+		<nav className='fixed z-10 bottom-0 left-1/2 -translate-x-1/2 pb-2'>
+			<div className='w-80 rounded-full p-2 bg-white/40 backdrop-blur'>
+				<ul className='flex items-center justify-between duration-100 transition-all '>
+					{pages.map((page) => (
+						<Link
+							key={page.name}
+							to={page.url}
+							className={`flex items-center justify-center gap-1 p-2 rounded-full overflow-hidden ${
 								location.pathname === page.url
-									? 'visible opacity-100'
-									: 'hidden opacity-0'
+									? 'text-slate-600 bg-[#dbb198] w-24 pr-3' 
+									: 'text-slate-950 w-9'
 							} duration-100 transition-all`}
 						>
-							{page.name}
-						</span>
-					</Link>
-				))}
-			</ul>
+							{location.pathname === page.url ? (
+								<page.icons.solid className='h-5 w-5 duration-100 transition-all' />
+							) : (
+								<page.icons.outline className='h-5 w-5 duration-100 transition-all' />
+							)}
+							<span
+								className={`text-sm capitalize lea ${
+									location.pathname === page.url
+										? 'visible opacity-100'
+										: 'hidden opacity-0'
+								} duration-100 transition-all`}
+							>
+								{page.name}
+							</span>
+						</Link>
+					))}
+				</ul>
+			</div>
 		</nav>
 	);
 };
